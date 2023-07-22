761c71984b9a9bc40a98ca81a6e5ee7056993a81
ArshGillGit <arshsingh387@gmail.com>
2023-07-22 16:21:01 -0700

    Updated backend TransactionService and added APIs for retrieving transaction data

--- technical/src/main/java/wsecu/technical/controllers/TransactionController.java
+++ technical/src/main/java/wsecu/technical/controllers/TransactionController.java
@@ -3,10 +3,13 @@ package wsecu.technical.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
 
 import wsecu.technical.services.TransactionService;
 import wsecu.technical.models.Transaction;
@@ -22,12 +25,22 @@ public class TransactionController {
     public ResponseEntity<?> saveTransaction(@RequestBody Transaction theTransaction) {
         try {
             Transaction transaction = transactionService.saveTransaction(theTransaction);
-
             return new ResponseEntity<>(transaction, HttpStatus.CREATED);
 
         } catch (IllegalArgumentException e) {
             return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
         }
+    }
+
+
+    @GetMapping("/{id}")
+    public Transaction getTransaction(@PathVariable Long theId) {
+        return transactionService.getSingleTransaction(theId);
+    }
+
+    @GetMapping
+    public List<Transaction> getAllTransactions(){
+        return transactionService.getAllTransaction();
     }
     
 }
